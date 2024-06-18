9d000bb0ea03bcf98ae2cdda5bb933938188b888
Loyola Leo Martin <masterdjmov@gmail.com>
2024-06-18 11:29:03 -0300

    agregando paquete os para obtener el hostname y activando escucha de server, metodo listen

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("prueba de configuración del package.json");
+//paquetes necesarios, os para el host y express para el manejo
+//de rutas y gestion en server
+const os = require('os');
+const express = require('express');
+//paquete para el manejo de rutas absolutas o relativas
+const path = require('path');
+const app = express();
+//inicializando una variable con el posible puerto
+const puerto = 3000;
+
+
+//activando escucha de puerto
+app.listen(puerto, (error) => {
+    if (error) {
+        console.log("Error: " + error);
+    } else {
+        const hostname = os.hostname();
+        console.log(`Server iniciado en http://${hostname}:${puerto}`);
+    }
+});
