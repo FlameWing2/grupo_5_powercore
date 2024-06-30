dc57e8c3d53fc3ae6199fd96340c991ec87be010
Loyola Leo Martin <masterdjmov@gmail.com>
2024-06-30 17:15:31 -0300

    Subiendo estructura de html

--- public/js/function.js
+++ public/js/function.js
@@ -1,1 +1,20 @@
-//alert("prueba de alert");
+document.addEventListener('DOMContentLoaded', (event) => {
+    // JavaScript code here if needed
+  });
+  
+  function toggleUserMenu() {
+    document.getElementById('user-menu').classList.toggle('show');
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.fa-user')) {
+      var dropdowns = document.getElementsByClassName("user-menu-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
