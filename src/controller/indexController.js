a343e3f0f0ad0767f19c8811beab4be3b61cb7c6
Loyola Leo Martin <masterdjmov@gmail.com>
2024-07-08 18:59:23 -0300

    mostrando productos mediante un json en data

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -1,7 +1,10 @@
 // asi vamos a manejar las rutas src/controllers/indexController.js
 //agrego la ruta para poder usar path
 const path = require('path');
+const archivo = require('node:fs');
 
+//cargo los productos por ahora con json
+const products = JSON.parse(archivo.readFileSync(path.resolve(__dirname, '../data/products.json'),"utf-8"));
 let indexController = {
     index: (req, res)=>{
         //res.sendFile(path.resolve(__dirname, '../views/index.html'));
@@ -9,7 +12,10 @@ let indexController = {
             titulo: "PowerCore",
             pie: "&copy; 2024 PowerCore. Trabajo grupal N&deg; 5."
         }
-        res.render('index',{'datos':datos});
+        
+       // console.log(products);
+        res.render('index',{'datos':datos,'products':products});
+
     }
 }
 
