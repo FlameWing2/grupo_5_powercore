9ef8c571db67babd1540d76f800c827ac0e16b74
Loyola Leo Martin <masterdjmov@gmail.com>
2024-07-19 14:15:58 -0300

    subiendo sweet alert en editproduct

--- src/routes/adminRouters.js
+++ src/routes/adminRouters.js
@@ -5,6 +5,7 @@ let adminController = require('../controller/adminController');
 //manejo de rutas segun clase 25 - MVC
 
 router.get('/edit',adminController.edit);
+router.post('/edit',adminController.edit);
 router.get('/create',adminController.create);
 router.get('/register',adminController.register);
 
