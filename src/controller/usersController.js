be5738cbe5a06ce979e2a51e05141aed50f3db22
MLM32 <marcosluismontilla@gmail.com>
2024-08-21 18:48:31 -0300

    Incorpore multer y bcryt

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -1,117 +1,120 @@
 // asi vamos a manejar las rutas src/controllers/indexController.js
 //agrego la ruta para poder usar path
-const path = require('path');
+const path = require("path");
 const crypto = require("node:crypto");
+const bcrypt = require("bcrypt");
 
 //llamo a los data que luego serán cambiados por las conexiones a BD
-const ParametrosGenerales = require('../config/parametros.js');
+const ParametrosGenerales = require("../config/parametros.js");
 const parametrosGenerales = new ParametrosGenerales();
 
-const Datasource = require('../services/datasource.js');
+const Datasource = require("../services/datasource.js");
 
 // Configurar el filePath
 const dataUsers = new Datasource(path.resolve(__dirname, "../data/users.json"));
 
 let usersController = {
-    //procesos de login
-    formLogin: (req, res)=>{ 
-        res.render("users/login",{
-            'datos':parametrosGenerales,
-            infoUsuario:(req.session.usuario)?req.session.usuario:'No hay Datos'
-        });
-    },
-    salir: (req, res)=>{
-        if(req.session.usuario != null ){
-            req.session.usuario = null;
-            res.redirect('/users/login');
-        }
-    },
-    validar: async (req, res)=>{
-        let { email, password,recordar } = req.body;
-        
-        
-        //consulto si existe este usuario
-        const usuarios = await dataUsers.load();
-        const existe = usuarios.find(user => {
-            if(user.email == req.body.email && user.password ==req.body.password){
-                return user;
-            }
-        })
-        if(existe){
-            req.session.usuario = existe;
-            //guardo el usario en cookie
-            if(recordar==='on'){
-                res.cookie('usuarioCokkie',existe.idUser, { maxAge: 60000, httpOnly: true });
-                console.log("se guardo cookie con "+ existe.idUser);
-            }
-            res.redirect('/');  //lo envio al index
-        }else{
-            parametrosGenerales.error = `Error, no se encontro las credenciales para el correo: <b>${email}</b>`;
-            res.render('users/login',{
-                'datos':parametrosGenerales,
-               infoUsuario:(req.session.usuario)?req.session.usuario:'No hay Datos'
-        });
-        }
-    },
-    //procesos de register
-    formRegister: (req, res)=>{
-        console.log("en get: " + parametrosGenerales.msg);
-        if(req.session.msg != null ){
-            parametrosGenerales.msg = req.session.msg;
-            req.session.msg = null;
-        }else{
-            parametrosGenerales.msg="";
-        }
-        console.log("en get: " + parametrosGenerales.msg);
-        res.render("users/register",{
-            'datos':parametrosGenerales,
-           infoUsuario:(req.session.usuario)?req.session.usuario:'No hay Datos'
-        });
-    },
-    createAvatar: (req, res)=>{
-      
-        /**si uso multer */
-        //console.log(req.file);
-        //if(req.file !== undefined) llego
-        res.render("users/register",{
-            'datos':parametrosGenerales,
-            infoUsuario:(req.session.usuario)?req.session.usuario:'No hay Datos'
-        });
-    },
-    createUser: async (req, res) => {
-        const { email, password,password2,nombre,apellido,dni, telefono } = req.body;
-
-        let nuevaUsuario={
-            idUser:crypto.randomUUID(),
-            apellido,
-            nombre,
-            dni,
-            telefono,
-            email,
-            password,
-            category:"Client",
-            imagen: "avatar-female.jpg",
-            direccion: "barrio xxx - calle xxx num xxxx"
-        }
-        const archivo = await dataUsers.load();
-        archivo.push(nuevaUsuario);
-        console.log(archivo);
-        await dataUsers.save(archivo);
-        // Aquí vamos a manejar los datos, por ejemplo, guardarlos en una base de datos
-        // o bien interactuar con ellos
-        //verificamos si llegan
-        //console.log('Datos recibidos:', req.body);
-        req.session.msg = `Gracias por registrarse en PowerCore,  ${nombre} ${apellido}, pronto le llegara un email a su correo`;
-        console.log("en post: " + req.session.msg);
-        // Luego redirigir o renderizar una vista con una respuesta adecuada
-        //res.send('Registro recibido: ' + nombre + " " + apellido);
-
-
-        res.redirect('/users/register');
-        
-        
-        //res.render("users/register",{'datos':parametrosGenerales});
+  //procesos de login
+  formLogin: (req, res) => {
+    res.render("users/login", {
+      datos: parametrosGenerales,
+      infoUsuario: req.session.usuario ? req.session.usuario : "No hay Datos",
+    });
+  },
+  salir: (req, res) => {
+    if (req.session.usuario != null) {
+      req.session.usuario = null;
+      res.redirect("/users/login");
     }
-}
+  },
+  validar: async (req, res) => {
+    let { email, password, recordar } = req.body;
+
+    //consulto si existe este usuario
+    const usuarios = await dataUsers.load();
+    const existe = usuarios.find((user) => {
+      if (user.email == req.body.email && user.password == req.body.password) {
+        return user;
+      }
+    });
+    if (existe) {
+      req.session.usuario = existe;
+      //guardo el usario en cookie
+      if (recordar === "on") {
+        res.cookie("usuarioCokkie", existe.idUser, {
+          maxAge: 60000,
+          httpOnly: true,
+        });
+        console.log("se guardo cookie con " + existe.idUser);
+      }
+      res.redirect("/"); //lo envio al index
+    } else {
+      parametrosGenerales.error = `Error, no se encontro las credenciales para el correo: <b>${email}</b>`;
+      res.render("users/login", {
+        datos: parametrosGenerales,
+        infoUsuario: req.session.usuario ? req.session.usuario : "No hay Datos",
+      });
+    }
+  },
+  //procesos de register
+  formRegister: (req, res) => {
+    console.log("en get: " + parametrosGenerales.msg);
+    if (req.session.msg != null) {
+      parametrosGenerales.msg = req.session.msg;
+      req.session.msg = null;
+    } else {
+      parametrosGenerales.msg = "";
+    }
+    console.log("en get: " + parametrosGenerales.msg);
+    res.render("users/register", {
+      datos: parametrosGenerales,
+      infoUsuario: req.session.usuario ? req.session.usuario : "No hay Datos",
+    });
+  },
+  createAvatar: (req, res) => {
+    /**si uso multer */
+    console.log(req.file);
+    //if(req.file !== undefined) llego
+    // res.render("users/register", {
+    //   datos: parametrosGenerales,
+    //   infoUsuario: req.session.usuario ? req.session.usuario : "No hay Datos",
+    // });
+  },
+  createUser: async (req, res) => {
+    const { email, password, password2, nombre, apellido, dni, telefono } =
+      req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    let nuevaUsuario = {
+      idUser: crypto.randomUUID(),
+      apellido,
+      nombre,
+      dni,
+      telefono,
+      email,
+      hashedPassword,
+      category: "Client",
+      imagen: "avatar-female.jpg",
+      direccion: "barrio xxx - calle xxx num xxxx",
+    };
+    const archivo = await dataUsers.load();
+    archivo.push(nuevaUsuario);
+    console.log(archivo);
+    await dataUsers.save(archivo);
+    // Aquí vamos a manejar los datos, por ejemplo, guardarlos en una base de datos
+    // o bien interactuar con ellos
+    //verificamos si llegan
+    //console.log('Datos recibidos:', req.body);
+    req.session.msg = `Gracias por registrarse en PowerCore,  ${nombre} ${apellido}, pronto le llegara un email a su correo`;
+    console.log("en post: " + req.session.msg);
+    // Luego redirigir o renderizar una vista con una respuesta adecuada
+    //res.send('Registro recibido: ' + nombre + " " + apellido);
+
+    res.redirect("/users/register");
+
+    //res.render("users/register",{'datos':parametrosGenerales});
+  },
+};
 
 module.exports = usersController;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,8 +1,24 @@
-const express = require('express');
+const express = require("express");
 let router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
+let usersController = require("../controller/usersController");
 
-let usersController = require('../controller/usersController');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const folder = path.join(__dirname, "../../public/images/userImages");
+    cb(null, folder);
+  },
+  filename: (req, file, cb) => {
+    console.log("FILE:", file);
+
+    const imageName = "user-" + Date.now() + path.extname(file.originalname);
+    cb(null, imageName);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 //manejo de rutas segun clase 25 - MVC
 /**
@@ -10,27 +26,24 @@ let usersController = require('../controller/usersController');
  * */
 
 /**ruta que muestra un formulario de login, formulario de recuperar clave */
-router.get('/login',usersController.formLogin);
+router.get("/login", usersController.formLogin);
 
 /**ruta que se encarga de validar el usuario, falta middleware */
-router.post('/login',usersController.validar);
+router.post("/login", usersController.validar);
 
 /**ruta que que se encarga de borrar la session*/
-router.get('/salir',usersController.salir);
+router.get("/salir", usersController.salir);
 /**------------------------------------------------------------------------------------------------------- */
-
-
-
 
 /**
  * Pagina de Registro
  * */
 
 /**ruta que muestra un formulario de registro */
-router.get('/register',usersController.formRegister);
+router.get("/register", usersController.formRegister);
 
 /**ruta que captura lo que la persona envia en el registro */
-router.post('/register',usersController.createUser);
+router.post("/register", upload.single("avatar"), usersController.createUser);
 
 /**ruta que captura el avatar del perfil(NO CREADO POR AHORA) */
 //router.post('/create-avatar', fileUpload.single('filenamehtml'),usersController.createAvatar);
