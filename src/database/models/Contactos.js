3013644ef5bd17581378cfb2decd1fbdc08e180c
Loyola Leo Martin <masterdjmov@gmail.com>
2024-09-27 00:45:32 -0300

    subiendo modelo de contactos

---
+++ src/database/models/Contactos.js
@@ -0,0 +1,55 @@
+module.exports = (Sequelize, DataTypes) => {
+    const Contactos = Sequelize.define('Contactos', {
+        id_contacto: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        apenom: {
+            type: DataTypes.STRING(75),
+            allowNull: true,
+            defaultValue: null
+        },
+        email: {
+            type: DataTypes.STRING(75),
+            allowNull: true,
+            defaultValue: null
+        },
+        mensaje: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            defaultValue: null
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: null
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: null
+        },
+        id_area: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: null
+        }
+    }, {
+        tableName: 'contactos',
+        timestamps: true, // Sequelize manejará createdAt y updatedAt
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    });
+
+    // Asociaciones si las necesitas
+    Contactos.associate = models => {
+        Contactos.belongsTo(models.Areas, {
+            as: 'area',
+            foreignKey: 'id_area'
+        });
+    };
+
+    return Contactos;
+};
